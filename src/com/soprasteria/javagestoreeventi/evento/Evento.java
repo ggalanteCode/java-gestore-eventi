51160145288feac4f57a7b236bd6732b494fde09
GALANTE Giovanni <giovanni.galante@soprasteria.com>
2022-10-11 20:10:24 +0200

    Gestione di eventi in Java!

---
+++ src/com/soprasteria/javagestoreeventi/evento/Evento.java
@@ -0,0 +1,12 @@
+package com.soprasteria.javagestoreeventi.evento;
+
+import java.time.LocalDate;
+
+public class Evento {
+	
+	private String titolo;
+	private LocalDate data;
+	private int numPostiTotale;
+	private int numPostiPrenotati;
+	
+}
