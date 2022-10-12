327f2ab132be6e8071dd93aefed1373ac6bc5d15
GALANTE Giovanni <giovanni.galante@soprasteria.com>
2022-10-12 16:48:35 +0200

    Concerto: metodi per formattare data, ora e prezzo e metodo toString()

--- src/com/soprasteria/javagestoreeventi/evento/Concerto.java
+++ src/com/soprasteria/javagestoreeventi/evento/Concerto.java
@@ -1,8 +1,10 @@
 package com.soprasteria.javagestoreeventi.evento;
 
 import java.math.BigDecimal;
+import java.text.DecimalFormat;
 import java.time.LocalDate;
 import java.time.LocalTime;
+import java.time.format.DateTimeFormatter;
 
 import com.soprasteria.javagestoreeventi.exceptions.DataPassataException;
 import com.soprasteria.javagestoreeventi.exceptions.NumeroPostiException;
@@ -17,7 +19,8 @@ public class Concerto extends Evento {
 			throws DataPassataException, NumeroPostiException {
 		super(titolo, data, numPostiTotale);
 		// TODO Auto-generated constructor stub
-		
+		this.ora = ora;
+		this.prezzo = prezzo;
 	}
 
 	public LocalTime getOra() {
@@ -34,6 +37,25 @@ public class Concerto extends Evento {
 
 	public void setPrezzo(BigDecimal prezzo) {
 		this.prezzo = prezzo;
+	}
+	
+	private String formattaDataEOra() {
+		DateTimeFormatter dtf = DateTimeFormatter.ofPattern("dd/MM/yyyy");
+		String dataFormattata = this.getData().format(dtf);
+		dtf = DateTimeFormatter.ofPattern("hh:mm:ss");
+		String oraFormattata = this.ora.format(dtf);
+		return "data: " + dataFormattata + ", ora formattata" + oraFormattata;
+	}
+	
+	private String formattaPrezzo() {
+		DecimalFormat df = new DecimalFormat("##.## €");
+		return "prezzo: " + df.format(this.getPrezzo());
+	}
+
+	@Override
+	public String toString() {
+		return formattaDataEOra() + ", " + getTitolo()+ ", " 
+				+ formattaPrezzo();
 	}
 
 }
